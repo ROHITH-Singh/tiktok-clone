71ae6448ec6286cf5aea53ddc2e00afca3548904
ROHITH-Singh <55267320+ROHITH-Singh@users.noreply.github.com>
2020-09-12 19:50:10 +0530

    mern stack build
    full stack

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { requirePropFactory } from "@material-ui/core";
+import React, {useEffect,useState,} from "react";
+import "./App.css";
+import Video from "./Video";
+import Axios from "./axios";
 function App() {
+
+ 
+
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await Axios.get('/v2/posts');
+      setVideos(response.data);
+      return response;
+    }
+    fetchPosts();
+  }, []);
+
+    
+  console.log(videos)
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="app">
+      <div className="app__videos">
+        {videos.map(({ url, channel, description, song, likes, messages, shares }) => (
+          <Video url={url}
+            channel={channel}
+            song={song}
+            likes={likes}
+            comments={messages}
+            shares={shares} />
+          
+       
+          // <Video
+          //   url={require("./first.mp4")}
+          //   channel={"rohitsingh"}
+          //   description={"follow me for amvs and montages "}
+          //   song={"remix audio by Rohitsingh"}
+          //   likes={1111}
+          //   comments={200}
+          //   shares={50}
+          // />
+        
+
+          // {/* app container */}
+          // {/* videos  */}
+          // {/* videos  */}
+          // {/* videos  */}
+          // {/* videos  */}
+          // {/* videos  */}
+          // {/* videos  */}
+          // {/* videos  */}
+        ))} </div></div>
+
+  )
 }
 
 export default App;
